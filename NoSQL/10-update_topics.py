dd16ef0396db219adb847c5c2833c75d2b414c5e
Nombreli <13177@holbertonstudents.com>
2026-05-06 01:27:13 +0200

    Update school topics using MongoDB update

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""function that changes all topics of a school document"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    Updates the topics of a school document
+    based on the school name
+    """
+    mongo_collection.update_many(
+        {"name": name},
+        {"$set": {"topics": topics}}
+    )
